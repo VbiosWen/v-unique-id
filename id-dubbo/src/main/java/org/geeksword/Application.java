5747926c54ae62c6c4b326c2d0241e87a0d4022d
温留杰 <wenliujie@qbb6.com>
2020-03-15 22:24:19 +0800

    add registry :home:

--- id-dubbo/src/main/java/org/geeksword/Application.java
+++ id-dubbo/src/main/java/org/geeksword/Application.java
@@ -1,23 +1,19 @@
 package org.geeksword;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import java.util.concurrent.locks.LockSupport;
 
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args){
-        SpringApplication springApplication = new SpringApplication(Application.class);
-        WebApplicationType webApplicationType = springApplication.getWebApplicationType();
-        ConfigurableApplicationContext context = springApplication.run(args);
-        if(webApplicationType == WebApplicationType.NONE){
-            LockSupport.park();
-        }
-        context.stop();
+
+//        SpringApplication springApplication = new SpringApplication(Application.class);
+//        WebApplicationType webApplicationType = springApplication.getWebApplicationType();
+//        ConfigurableApplicationContext context = springApplication.run(args);
+//        if(webApplicationType == WebApplicationType.NONE){
+//            LockSupport.park();
+//        }
+//        context.stop();
     }
 }
